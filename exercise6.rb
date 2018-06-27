ba37f0c64adb56c0943a2e347be6e0a5e5b4ea68
Gabriel Alarcon <galarconesp@gmail.com>
2018-06-27 08:43:11 -0400

    commit 03

---
+++ exercise6.rb
@@ -0,0 +1,27 @@
+class Product
+  def initialize(name, large, medium, small, xsmall)
+    @name = name
+    @large = large.to_i
+    @medium = medium.to_i
+    @small = small.to_i
+    @xsmall = xsmall.to_i
+  end
+
+  def promedio
+    prom = (@large + @medium + @small + @xsmall) / 4.0
+    puts "El promedio de precios de #{@name} es #{prom}"
+  end
+end
+
+products_list = []
+data = []
+File.open('catalogo.txt', 'r') { |file| data = file.readlines }
+data.each do |prod|
+  ls = prod.split(', ')
+  # products_list << Product.new(ls[0], ls[1], ls[2], ls[3], ls[4])
+  products_list << Product.new(*ls)
+end
+
+print products_list
+
+products_list.each(&:promedio)
